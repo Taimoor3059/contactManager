62be412510d1da63654d19798be2b7570048693d
Taimoor Tahir <taimoorhtahir@gmail.com>
2020-02-28 12:56:32 -0600

    fixing dependency

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-//import { Card } from "native-base";
-//import { Entypo } from " @expo/vector-icons";
-//import { getSupportedVideoFormats } from 'expo/build/AR';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, AsyncStorage } from 'react-native';
+import { Card } from "native-base";
+import { Ionicons } from " @expo/vector-icons";
+
 
 export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
 
+  static navigationOptions = {
+    title: "Contact App"
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to HomeScreen</Text>
+        <TouchableOpacity
+        style={styles.floatButton}
+          onPress={ () => {
+            this.props.navigations.navigate("Add");
+          } }
+        >
+          <Ionicons
+            name="md-checkmark-circle"
+            size={32}
+            color="green" 
+          />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -18,10 +38,47 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  listItem: {
+    flexDirection: "row",
+    padding: 20
+  },
+  iconContainer: {
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#B83227",
+    borderRadius: 100
+  },
+  contactIcon: {
+    fontSize: 28,
+    color: "#fff"
+  },
+  infoContainer: {
+    flexDirection: "column"
+  },
+  infoText: {
+    fontSize: 16,
+    fontWeight: "400",
+    paddingLeft: 10,
+    paddingTop: 2
+  },
+  floatButton: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 60,
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    height: 60,
+    backgroundColor: "#B83227",
+    borderRadius: 100
+  }
 });
+
 
 
