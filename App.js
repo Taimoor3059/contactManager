5edae18bbc29788770510e25b999660b64bca1ab
Taimoor Tahir <taimoorhtahir@gmail.com>
2020-02-28 11:52:17 -0600

    screen options

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -9,24 +8,23 @@ import AddNewContactScreen from './screens/AddNewContactScreen';
 import EditContactScreen from './screens/EditContactScreen';
 import ViewContactScreen from './screens/ViewContactScreen';
 
-//class HomeScreen extends React.Component {
-  //render() {
-   // return (
-    //  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-     //   <Text>Home Screen</Text>
-     // </View>
- //   );
-//  }
-//}
-
-
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#b83227"
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            color: "#fff"
+          }
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Add" component={AddNewContactScreen} />
         <Stack.Screen name="Edit" component={EditContactScreen} />
