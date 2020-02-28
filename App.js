f74f06bdb161a00f4fce5ad5a0f2ff53beda871d
Taimoor Tahir <taimoorhtahir@gmail.com>
2020-02-28 11:45:26 -0600

    adding screens
    that took some time

--- App.js
+++ App.js
@@ -1,32 +1,43 @@
 
-//import screens
-import HomeScreen from "./screens/HomeScreen";
-import EditContactScreen from "./screens/EditContactScreen";
-import AddNewContactScreen from "./screens/AddNewContactScreen";
-import ViewContactScreen from "./screens/ViewContactScreen";
-
-//import react navigation
+import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import HomeScreen from "./screens/HomeScreen";
+import AddNewContactScreen from './screens/AddNewContactScreen';
+import EditContactScreen from './screens/EditContactScreen';
+import ViewContactScreen from './screens/ViewContactScreen';
 
-const stack = createStackNavigator();
+//class HomeScreen extends React.Component {
+  //render() {
+   // return (
+    //  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     //   <Text>Home Screen</Text>
+     // </View>
+ //   );
+//  }
+//}
 
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="HomeScreen" Component={HomeScreen} />
-          <Stack.Screen name="AddNewContactScreen" Component={AddNewContactScreen} />
-          <Stack.Screen name="EditContactScreen" component={EditContactScreen} />
-          <Stack.Screen name="ViewContactScreen" component={ViewContactScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>  
-    );
-  }
-  
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add" component={AddNewContactScreen} />
+        <Stack.Screen name="Edit" component={EditContactScreen} />
+        <Stack.Screen name="View" component={ViewContactScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
+
+
 
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//import { Card } from "native-base";
+//import { Entypo } from " @expo/vector-icons";
+//import { getSupportedVideoFormats } from 'expo/build/AR';
 
 export default class HomeScreen extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>HomeScreen</Text>
+        <Text>Welcome to HomeScreen</Text>
       </View>
     );
   }
-  
 }
 
 const styles = StyleSheet.create({
