f35d30d08c5b6f1704df5a91ac1daa011de0587e
Taimoor Tahir <taimoorhtahir@gmail.com>
2020-03-01 23:41:44 -0600

    editing/functions to the AddContactScree

--- screens/AddNewContactScreen.js
+++ screens/AddNewContactScreen.js
@@ -17,11 +17,61 @@ export default class AddNewContactScreen extends React.Component {
   static navigationOptions = {
     title: "Contact App"
   }
+
+  saveContact = async () => {
+    if (
+      this.state.firstName !== "empty" &&
+      this.state.lastName !== "empty" &&
+      this.state.phoneNumber !== "empty" &&
+      this.state.email !== "empty" &&
+      this.state.address !== "empty"
+    ) {
+       var contact = {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        phoneNumber: this.state.phoneNumber,
+        email: this.state.email,
+        address: this.state.address
+       }
+
+       await AsyncStorage.setItem( Date.now().toString(),
+       JSON.stringify(contact)
+       )
+       .then( () => {
+         this.props.navigation.goBack();
+       })
+       .catch( error => {
+         console.log(error)
+       })
+    } else {
+      Alert.alert("All fields are required!")
+    }
+  }
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>AddNewContactScreen</Text>
-      </View>
+      <TouchableWithoutFeedback
+        onPress = { () => {
+          Keyboard.dismiss
+        }}
+      
+      >
+      <ScrollView style={styles.container}>
+        <Form>
+          <Item style={styles.inputItem}>
+            <label>First Name</label>
+            <input 
+            autoCorrect={false}
+            autoCapitalize="none"
+            KeyboardType="decimal-pad"
+            onChangeText={ firstName => this.setState({firstName}) }
+            />
+          </Item>
+        </Form>
+      </ScrollView>
+
+      </TouchableWithoutFeedback>
     );
   }
   
@@ -30,8 +80,23 @@ export default class AddNewContactScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    margin: 10,
+    height: 500
   },
+  inputItem: {
+    margin: 10
+  },
+  button: {
+    backgroundColor: "#B83227",
+    marginTop: 40
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold"
+  },
+  empty: {
+    height: 500,
+    backgroundColor: "#FFF"
+  }
 });
