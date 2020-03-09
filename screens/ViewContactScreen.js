3b42c489ce9bd6c88032d4e400ac4b213f6cbf44
Taimoor Tahir <taimoorhtahir@gmail.com>
2020-03-09 11:33:34 -0500

    designing the View Contact screen

--- screens/ViewContactScreen.js
+++ screens/ViewContactScreen.js
@@ -1,7 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Linking, Platform, Alert, AsyncStorage } from 'react-native';
+import { Card, CardItem } from "native-base";
+import { Entypo } from "@expo/vector-icons"
 
 export default class ViewContactScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "DummyText",
+      lastName: "DummyText",
+      phoneNumber: "DummyText",
+      email: "DummyText",
+      address: "DummyText",
+      key:"DummyText"
+    }
+  }
+
+  static navigationOptions = {
+    title: "View Contact"
+  };
+
+  componentDidMount() {
+    const { navigation } = this.props;
+    navigation.addListener("WillFocus", () => {
+      var key = this.props.navigation.getParam("key", "");
+    })
+  }
+
+  getContact = async key => {
+    await AsyncStorage.getItem(key)
+  }
+  
+
+
+
+
+
+
   render() {
     return (
       <View style={styles.container}>
